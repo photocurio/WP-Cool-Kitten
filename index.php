07058cb2e24f0a9271b660a64615c55c1448932a
Peter Mumford <peter@topheavypilesofbooks.com>
2014-02-02 16:11:21 -0500

    Major re-factoring
    Reduced theme to 4 files, all in the theme directory:
    style.css
    index.php
    functions.php
    coolkitten.js
    
    Removed fonts, set theme to Helvetica.

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php get_header(); ?>
+<!DOCTYPE HTML>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
+<head>
+	<meta http-equiv="Content-Type" charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+	<title><?php bloginfo( 'name' ); ?></title>
+	<link rel="stylesheet" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+	<?php wp_head(); ?>
+</head>
 
+<body <?php body_class(); ?>>
 	<div class="kitty-menu clearfix">
 		<div class="container clearfix relative logo-header">
 			<div id="logo" class="grid_12">
@@ -22,17 +34,14 @@
 		<div class="secondary-menu-slide">
 			<!--div class="container relative" id="secondary-menu"><?php //dynamic_sidebar('secondary'); ?></div-->
 		</div>
-		<?php //START THE LOOP
-		if ( have_posts() ) : 
-			while ( have_posts() ) : 
-			the_post(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		
 		<div class="slide" 
 		id="slide<?php echo get_the_ID(); ?>" 
 		data-slide="<?php echo get_the_ID(); ?>" 
 		data-stellar-background-ratio="0.5" 
 		style="<?php if ( get_post_meta( get_the_ID(), 'background-color', true ) ) : ?> background-color:<?php echo get_post_meta( get_the_ID(), 'background-color', true ) ?>;<?php endif; ?>" >
-		<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'original' ); ?>
+		<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'slide-image' ); ?>
 		<div class="background-div"><img src="<?php echo $src[0]; ?>"></div>
 						
 			<div class="container clearfix">
@@ -58,4 +67,6 @@
 		
 	</div><!-- PAGE -->
 	
-<?php get_footer(); ?>
+	<?php wp_footer(); ?>
+</body>
+</html>
